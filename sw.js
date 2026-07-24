ddcc5706142d1f91868a27e6e7987da6aab81f37
magdielperezm90 <magdielperezm90@gmail.com>
2026-07-24 01:40:19 -0700

    moremoremore

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bot-qqq-v13';
+const CACHE = 'bot-qqq-v15';
 const SHELL = ['./bot.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
